128bc1c0c7931befe8a6c34d9ada32260b7597b0
Tenderest <2087886585@qq.com>
2020-01-14 21:36:47 +0800

    2020/01/14 :tada: 3.5 chartype.cpp

---
+++ Chapter3/chartype.cpp
@@ -0,0 +1,12 @@
+// chartype.cpp -- the char type
+#include <iostream>
+int main(void)
+{
+        using namespace std;
+        char ch; // 声明字符变量 声明一个char变量
+        cout << "Tnter a character: " << endl;
+        cin >> ch;
+        cout << "Hola: ";
+        cout << "Thank you for the " << ch << " character." << endl;
+        return 0;
+}

433ed73a9580f627f9e99dc74757663a4ad8da3e
Tenderest <2087886585@qq.com>
2020-01-16 15:56:24 +0800

    2020/01/16 :tada: 3.7 bondini.cpp

---
+++ Chapter3/bondini.cpp
@@ -0,0 +1,13 @@
+// bondind.cpp -- using cscape sequences
+#include <iostream>
+int main(void)
+{
+        using namespace std;
+        cout << "\aOperation \"HyperHyper\" is now activated!\n";
+        cout << "Enter your agent code:________\b\b\b\b\b\b\b\b";
+        long code;
+        cin >> code;
+        cout << "\aYou entered " << code << "...\n";
+        cout << "\aCode verified! Proceed with Plan Z3!\n";
+        return 0;
+}

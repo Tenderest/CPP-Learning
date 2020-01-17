7ae80e2f9feb510d43843f1c683a59be88a8975c
Tenderest <2087886585@qq.com>
2020-01-17 20:47:37 +0800

    2020/01/17 :tada: 4.5 instr3.cpp

---
+++ Chapter4/instr3.cpp
@@ -0,0 +1,16 @@
+// instr3.cpp -- reading more than one word with get() & get()
+#include <iostream>
+int main(void)
+{
+        using namespace std;
+        const int ArSize = 20;
+        char name[ArSize];
+        char dessert[ArSize];
+        cout << "Enter your name:\n";
+        cin.get(name, ArSize).get(); // 读取字符串，换行符 reads string, newline
+        cout << "Enter your favorite dessert:\n";
+        cin.get(dessert, ArSize).get();
+        cout << "I have some delicious " << dessert;
+        cout << " for you, " << name << ".\n";
+        return 0;
+}
